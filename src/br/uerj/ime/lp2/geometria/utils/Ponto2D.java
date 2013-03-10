9fb3c5300c7326ec6b889ac6ddbd9e8ebbf2544c
Thiago Farina <tfarina@chromium.org>
2013-03-10 18:07:39 -0300

    Implement subtrair() function in Ponto2D class

--- src/br/uerj/ime/lp2/geometria/utils/Ponto2D.java
+++ src/br/uerj/ime/lp2/geometria/utils/Ponto2D.java
@@ -3,23 +3,27 @@ package br.uerj.ime.lp2.geometria.utils;
 public class Ponto2D {
 	private float x;
 	private float y;
-	
+
 	public Ponto2D(float x, float y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public Ponto2D() {
 		this(0f, 0f);
 	}
-	
+
 	public double distancia(Ponto2D ponto) {
 		double d1 = x - ponto.x;
 		double d2 = y - ponto.y;
 		return Math.sqrt(d1 * d1 + d2 * d2);
 	}
 
-	public float getX() {
+	public Vetor2D subtrair(Ponto2D ponto) {
+		return new Vetor2D(new Ponto2D(x - ponto.x, y - ponto.y));
+	}
+
+    public float getX() {
 		return x;
 	}
 
